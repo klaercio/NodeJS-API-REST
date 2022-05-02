b0af807921fe80d5c6e7e889dce6202db2f736b8
klaercio <klaerciosilva@gmail.com>
2022-05-01 23:09:30 -0300

    Colocando o projeto no padrão MVC

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
@@ -10,22 +11,9 @@ db.once("open", ()  => {
 
 const app = express();
 
+routes(app);
+
 app.use(express.json());
-
-/* const livros = [
-    {id: 1, 'titulo' : 'senhor dos aneis'}, 
-    {id: 2, 'titulo' : 'o hobbit'}
-] */
-
-app.get('/', (req,res) => {
-    res.status(200).send('Curso de Node');
-})
-
-app.get('/livros', (req,res) => {
-    livros.find((err, livros)=> {
-        res.status(200).json(livros);
-    });
-})
 
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
